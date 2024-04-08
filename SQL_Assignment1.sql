86550610564fd935680b4764908e62c702102997
Procopii Maria <77497709+MariaProcopii@users.noreply.github.com>
2024-04-08 17:15:42 +0300

    Update SQL_Assignment1.sql

--- SQL_Assignment1.sql
+++ SQL_Assignment1.sql
@@ -1,13 +1,14 @@
+CREATE TABLE VehicleTypes (
+    id UUID PRIMARY KEY,
+    car_model VARCHAR(255) NOT NULL
+);
+
 CREATE TABLE drivers (
     id UUID PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE
-);
-
-CREATE TABLE vehicles (
-    driver_id UUID PRIMARY KEY REFERENCES drivers(id),
-    car_model VARCHAR(255) NOT NULL,
-    license_number VARCHAR(50) NOT NULL
+    email VARCHAR(255) NOT NULL UNIQUE,
+    license_number VARCHAR(50) NOT NULL,
+    vehicle_type_id UUID REFERENCES VehicleTypes(id)
 );
 
 CREATE TABLE passengers (
@@ -16,18 +17,25 @@ CREATE TABLE passengers (
     email VARCHAR(255) NOT NULL UNIQUE
 );
 
-CREATE TABLE passenger_payments (
-    passenger_id UUID PRIMARY KEY REFERENCES passengers(id),
+CREATE TABLE PaymentMethods (
+    id UUID PRIMARY KEY,
     payment_method VARCHAR(255) NOT NULL
 );
 
+CREATE TABLE passenger_payments (
+    passenger_id UUID REFERENCES passengers(id),
+    payment_method_id UUID REFERENCES PaymentMethods(id),
+    PRIMARY KEY (passenger_id, payment_method_id)
+);
+
+
 CREATE TABLE rides (
-    id UUID PRIMARY KEY,
-    date_of_ride TIMESTAMP NOT NULL,
-    destination_from VARCHAR(255) NOT NULL,
-    destination_to VARCHAR(255) NOT NULL,
-    available_seats INTEGER NOT NULL,
-    owner_id UUID REFERENCES drivers(id) ON DELETE CASCADE
+   id UUID PRIMARY KEY,
+   date_of_ride TIMESTAMP NOT NULL,
+   destination_from VARCHAR(255) NOT NULL,
+   destination_to VARCHAR(255) NOT NULL,
+   available_seats INTEGER NOT NULL,
+   owner_id UUID REFERENCES drivers(id) ON DELETE CASCADE
 );
 
 CREATE TABLE ride_passengers (
